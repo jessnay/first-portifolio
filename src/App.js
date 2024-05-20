34456d0c18516d2faae85d590cbcf4c0a98be837
Jessica <jessica.nayara.pereira@hotmail.com>
2024-05-20 13:26:31 -0400

    Inicial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Ola, mundo! </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Primeira pagina
         </p>
         <a
           className="App-link"
